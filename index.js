e8b92e7c9af2185fc44c6e3387b1ffa979d0c74f
sachidananda-panigrahi <snpanigrahi88@gmail.com>
2023-01-12 12:33:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-};
+function doReq(n1, n2) {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+  return new Promise((resolve, reject) => {
+    fetch(
+      `https://ac74-49-36-56-167.in.ngrok.io/api/add?num1=${n1}&num2=${n2}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        resolve(result?.sum);
+      })
+      .catch((error) => {
+        console.log('error', error);
+        reject(error);
+      });
+  });
+}
 
-fetch(
-  'https://9432-49-36-56-167.in.ngrok.io/api/add?num1=2&num2=3',
-  requestOptions
-)
-  .then((response) => response.json())
-  .then((result) => console.log(result?.sum))
-  .catch((error) => console.log('error', error));
+async function getSumOfTwoNum(n1, n2) {
+  const sum = await doReq(n1, n2);
+  console.log(sum);
+}
+
+getSumOfTwoNum(3, 5);
